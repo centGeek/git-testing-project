aab68c8f738a746ba8f70a51c428bd04b695f35e
Roman <centkowski.lukasz03@gmail.com>
2023-04-22 13:46:04 +0200

    AlgorithmMethod

--- src/AlgorithmEx.java
+++ src/AlgorithmEx.java
@@ -1,2 +1,14 @@
+import java.util.stream.IntStream;
+
 public class AlgorithmEx {
+    public static float pow(int x, int y){
+        if(y<0){
+            return pow(x, y+1) * 1/x;
+        }else if(y>0){
+            return pow(x,y-1) * x;
+        } else {
+            return 1;
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(pow(3, -5));
     }
 }
